f9f98294bccf08714a9b934234c05f51c78d15d0
sulfiyasiyad <sulfiyacs16@gmail.com>
2024-04-15 21:01:03 +0530

    3

--- app1/views.py
+++ app1/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request,'home.html')
 def dashboard(request):
     return render(request,'dashboard.html')
+def add_cards(request):
+    return render(request,'add_card.html')
