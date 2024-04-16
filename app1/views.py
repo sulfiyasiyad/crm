adfa195f121f133f5f62ab9881f237d8f1fe032e
sulfiyasiyad <sulfiyacs16@gmail.com>
2024-04-16 21:12:36 +0530

    4

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app1.models import card
+
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 def dashboard(request):
     return render(request,'dashboard.html')
-def add_cards(request):
+def add_card(request):
     return render(request,'add_card.html')
+def add_card1(request):
+    if request.method=='POST':
+        name=request.POST['name'] 
+        a=card(name=name)
+        a.save()
+    return redirect('add_card')
+def view_card(request):
+    a=card.objects.all().filter(status=0)
+    return render(request,'view_card.html',{'a':a})
+def Archeive(request,pk):
+    a=card.objects.get(id=pk)
+    print(a)
+    a.status=1
+    a.save()
+    return redirect('view_card')
