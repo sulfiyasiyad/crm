35dcc989e8c414104bbe34821a7302ee3549f717
sulfiyasiyad <sulfiyacs16@gmail.com>
2024-04-20 10:11:02 +0530

    6

--- app1/urls.py
+++ app1/urls.py
@@ -4,9 +4,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('dashboard',views.dashboard,name='dashboard'),
     path('add_card',views.add_card,name='add_card'),
-    path('add_card1',views.add_card1,name='add_card1'),
-    path('view_card',views.view_card,name='view_card'),
-    path('Archeive<int:pk>',views.Archeive,name='Archeive'),
+    path('home',views.home,name='home'),
     path('view_Archeive',views.view_Archeive,name='view_Archeive'),
 
     
--- app1/views.py
+++ app1/views.py
@@ -9,20 +9,6 @@ def dashboard(request):
     return render(request,'dashboard.html')
 def add_card(request):
     return render(request,'add_card.html')
-def add_card1(request):
-    if request.method=='POST':
-        name=request.POST['name'] 
-        a=card(name=name)
-        a.save()
-    return redirect('add_card')
-def view_card(request):
-    a=card.objects.all().filter(status=0)
-    return render(request,'view_card.html',{'a':a})
-def Archeive(request,pk):
-    a=card.objects.get(id=pk)
-    print(a)
-    a.status=1
-    a.save()
-    return redirect('view_card')
+
 def view_Archeive(request):
     return render(request,'view_archeive.html')
